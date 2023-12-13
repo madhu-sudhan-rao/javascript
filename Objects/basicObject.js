ca2737bd9bbf38dba3aa24d134c72a2cc8903c98
unknown <madhuspediredla@gmail.com>
2023-12-13 15:09:01 +0530

    Objects

--- Objects/basicObject.js
+++ Objects/basicObject.js
@@ -8,6 +8,11 @@ user.isAdmin = true; // Add property
 delete user.isAdmin; // Delete property 
 
 console.log('user: ', user); // Display object
-console.log('Age (way-1)',user[`age`]); // Display value of a particular property (way-1)
-console.log('Age (way-2)',user.age); // Display value of a particular property (way-2)
+console.log('Age (way-1) ->',user[`age`]); // Display value of a particular property (way-1)
+console.log('Age (way-2) ->',user.age); // Display value of a particular property (way-2)
 
+user.sayHi = function () {
+    console.log('Hi')
+}
+
+user.sayHi()
