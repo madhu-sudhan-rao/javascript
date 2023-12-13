380152de8dbece4ffa61ee0e7dd9f7ec59a243c8
unknown <madhuspediredla@gmail.com>
2023-12-13 15:08:51 +0530

    Objects

--- Objects/funcObject.js
+++ Objects/funcObject.js
@@ -11,8 +11,22 @@ console.log('user: ', user);
 console.log('user: ', user);
 console.log('name' in user)
 console.log(user.gender)
+let u2 = user;
+console.log('u2: ', u2);
+
+user.name = 'Ricky';
+console.log(u2)
 
 for(key in user) {
     console.log(key);
     console.log(user[key]);
 }
+
+console.log('-----------')
+
+let u3= {};
+Object.assign(u3,user)
+console.log('u3: ', u3);
+
+let u4 = Object.assign({}, user)
+console.log('u4: ', u4);
