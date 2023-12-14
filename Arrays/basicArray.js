3ab5be83e07793a7b39207177bd6f3860aef023d
unknown <madhuspediredla@gmail.com>
2023-12-14 14:33:03 +0530

    Arrays

---
+++ Arrays/basicArray.js
@@ -0,0 +1,7 @@
+let random = ["package", "needle", "fallen", "growth", "if", function() { console.log('This is a function in an array') }];
+
+console.log(random);
+console.log( random[2] );
+console.log( random.length );
+console.log( random[random.length - 1]() );
+
