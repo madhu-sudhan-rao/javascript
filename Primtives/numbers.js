3c376ae111a1b9bbc786345ae8a6837143f9526e
madhu-888 <madhuspediredla@gmail.com>
2024-01-17 23:13:46 +0530

    primitives started

---
+++ Primtives/numbers.js
@@ -0,0 +1,5 @@
+let billion = 1_000_000_000; // syntactic sugar
+console.log( billion );
+
+let billion1 = 1e9;
+let billion7 = 7.3e9;
---
+++ Primtives/prim1.js
@@ -0,0 +1,3 @@
+let str = "Hello";
+str.test = 5;
+console.log(str.test)
